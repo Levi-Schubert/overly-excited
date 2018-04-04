50c30ddb63b6ba7ee6365331b1531e85beabcaf4
Levi Schubert <me@levischubert.com>
2018-04-04 15:48:23 -0500

    made more efficient (removed unnecessary for loop)

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ console.log("Hello, world!");
 
 let addExcitement = (words, punct) => {
     let build = "";
-    var third = 1;
+    let third = 1;
     for(let i = 0; i < words.length; i += 1){
-        for(let j = 0; j <= i; j += 1){
-            if(third % 3 == 0 && third != 0){
-                let exclame = punct;
-                if(third/3 > 1){
-                    for(let k = third/3; k > 1; k -= 1){
-                        exclame += punct;
-                    }
+        
+        if(third % 3 == 0 && third != 0){
+            let exclame = punct;
+            if(third/3 > 1){
+                for(let k = third/3; k > 1; k -= 1){
+                    exclame += punct;
                 }
-                build += words[j] + exclame + " ";
-                third  += 1;
-                
-            }else{
-                build += words[j] + " ";
-                third += 1;
             }
+            build += words[i] + exclame + " ";
+            third  += 1;
+            
+        }else{
+            build += words[i] + " ";
+            third += 1;
         }
+        console.log(build);
     }
-    console.log(build)
+   
 };
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 let sentenceTwo = ["The","quick","brown","fox","jumps","over","the","lazy","dog"];
